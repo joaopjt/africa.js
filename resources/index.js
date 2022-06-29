4ef1864b6b237025b68609ca739078b08708069a
Joao Julio <joaopjt@gmail.com>
2022-06-29 15:09:07 -0300

    Added africa.js API resources.

--- resources/index.js
+++ resources/index.js
@@ -20,9 +20,191 @@ export default class Africa {
     return this;
   }
 
+  char(size) {
+    this.spacer();
+    this.collumn_string += `char(${size})`;
+
+    return this;
+  }
+
   varchar(size) {
     this.spacer();
     this.collumn_string += `varchar(${size})`;
+
+    return this;
+  }
+
+  binary(size) {
+    this.spacer();
+    this.collumn_string += `BINARY(${size})`;
+
+    return this;
+  }
+
+  varbinary(size) {
+    this.spacer();
+    this.collumn_string += `VARBINARY(${size})`;
+
+    return this;
+  }
+
+  tinyblob() {
+    this.spacer();
+    this.collumn_string += `TINYBLOB`;
+
+    return this;
+  }
+
+  tinytext() {
+    this.spacer();
+    this.collumn_string += `TINYTEXT`;
+
+    return this;
+  }
+
+  tinyint() {
+    this.spacer();
+    this.collumn_string += `TINYINT`;
+
+    return this;
+  }
+
+  bool() {
+    this.spacer();
+    this.collumn_string += `BOOL`;
+
+    return this;
+  }
+
+  boolean() {
+    this.spacer();
+    this.collumn_string += `BOOLEAN`;
+
+    return this;
+  }
+
+  smallint(size) {
+    this.spacer();
+    this.collumn_string += `SMALLINT(${size})`;
+
+    return this;
+  }
+
+  mediumint(size) {
+    this.spacer();
+    this.collumn_string += `MEDIUMINT(${size})`;
+
+    return this;
+  }
+
+  int(size) {
+    this.spacer();
+    this.collumn_string += `INT(${size})`;
+
+    return this;
+  }
+
+  integer(size) {
+    this.spacer();
+    this.collumn_string += `INTEGER(${size})`;
+
+    return this;
+  }
+
+  bigint(size) {
+    this.spacer();
+    this.collumn_string += `BIGINT(${size})`;
+
+    return this;
+  }
+
+  float(size, d) {
+    this.spacer();
+    this.collumn_string += `FLOAT(${size}, ${d})`;
+
+    return this;
+  }
+
+  double(size, d) {
+    this.spacer();
+    this.collumn_string += `DOUBLE(${size}, ${d})`;
+
+    return this;
+  }
+
+  double_precision(size, d) {
+    this.spacer();
+    this.collumn_string += `DOUBLE PRECISION(${size}, ${d})`;
+
+    return this;
+  }
+
+  decimal(size, d) {
+    this.spacer();
+    this.collumn_string += `DECIMAL(${size}, ${d})`;
+
+    return this;
+  }
+
+  text() {
+    this.spacer();
+    this.collumn_string += `TEXT`;
+
+    return this;
+  }
+
+  bit(size) {
+    this.spacer();
+    this.collumn_string += `BIT(${size})`;
+
+    return this;
+  }
+
+  blob(size) {
+    this.spacer();
+    this.collumn_string += `BLOB(${size})`;
+
+    return this;
+  }
+
+  mediumtext(size) {
+    this.spacer();
+    this.collumn_string += `MEDIUMTEXT`;
+
+    return this;
+  }
+
+  mediumblob(size) {
+    this.spacer();
+    this.collumn_string += `MEDIUMBLOB`;
+
+    return this;
+  }
+
+  longtext() {
+    this.spacer();
+    this.collumn_string += `LONGTEXT`;
+
+    return this;
+  }
+
+  longblob() {
+    this.spacer();
+    this.collumn_string += `LONGBLOB`;
+
+    return this;
+  }
+
+  enum(list) {
+    this.spacer();
+    this.collumn_string += `ENUM(${list})`;
+
+    return this;
+  }
+
+  set(list) {
+    this.spacer();
+    this.collumn_string += `SET(${list})`;
 
     return this;
   }
@@ -39,6 +221,41 @@ export default class Africa {
   primary_key() {
     this.spacer();
     this.collumn_string += 'PRIMARY KEY';
+
+    return this;
+  }
+
+  date() {
+    this.spacer();
+    this.collumn_string += 'DATE';
+
+    return this;
+  }
+
+  datetime(format) {
+    this.spacer();
+    this.collumn_string += `DATETIME(${format})`;
+
+    return this;
+  }
+
+  timestamp(format) {
+    this.spacer();
+    this.collumn_string += `TIMESTAMP(${format})`;
+
+    return this;
+  }
+
+  time(format) {
+    this.spacer();
+    this.collumn_string += `TIME(${format})`;
+
+    return this;
+  }
+
+  year() {
+    this.spacer();
+    this.collumn_string += `YEAR`;
 
     return this;
   }
