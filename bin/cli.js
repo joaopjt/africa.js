50ce748378d7ff3d0ff2fe8f9240a3440d6be3a5
Joao Julio <joaopjt@gmail.com>
2022-08-24 21:09:18 -0300

    minor fix

--- bin/cli.js
+++ bin/cli.js
@@ -1,3 +1,14 @@
+/**
+ * Copyright (c) África.js
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @format
+ */
+
+'use strict';
+
 require('dotenv').config();
 
 const moment = require('moment');
