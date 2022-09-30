8b6f3d76bedfc75ee42df76f330d8abc7d1be4c8
Joao Julio <joaopjt@gmail.com>
2022-09-30 16:16:41 -0300

    version 1.8.5

--- resources/index.js
+++ resources/index.js
@@ -8,274 +8,274 @@ import SQLite from './api/sqlite';
 import SQLServer from './api/sqlserver';
 
 class Africa {
-  constructor(space) {
-    this.collumn_string = '';
+  constructor() {
+    this.string = '';
   }
 
   static spacer() {
-    if (this.collumn_string) this.collumn_string += ' ';
+    if (this.string) this.string += ' ';
   }
 
   static int() {
     this.spacer();
-    this.collumn_string += 'int';
+    this.string += 'int';
 
-    return this || this.collumn_string;
+    return this || this.string;
   }
 
   static char(size) {
     this.spacer();
-    this.collumn_string += `char(${size})`;
+    this.string += `char(${size})`;
 
-    return this || this.collumn_string;
+    return this || this.string;
   }
 
   static varchar(size = 255) {
     this.spacer();
-    this.collumn_string += `varchar(${size})`;
+    this.string += `varchar(${size})`;
 
-    return this || this.collumn_string;
+    return this || this.string;
   }
 
   static binary(size) {
     this.spacer();
-    this.collumn_string += `BINARY(${size})`;
+    this.string += `BINARY(${size})`;
 
-    return this || this.collumn_string;
+    return this || this.string;
   }
 
   static varbinary(size) {
     this.spacer();
-    this.collumn_string += `VARBINARY(${size})`;
+    this.string += `VARBINARY(${size})`;
 
-    return this || this.collumn_string;
+    return this || this.string;
   }
 
   static tinyblob() {
     this.spacer();
-    this.collumn_string += `TINYBLOB`;
+    this.string += `TINYBLOB`;
 
-    return this || this.collumn_string;
+    return this || this.string;
   }
 
   static tinytext() {
     this.spacer();
-    this.collumn_string += `TINYTEXT`;
+    this.string += `TINYTEXT`;
 
-    return this || this.collumn_string;
+    return this || this.string;
   }
 
   static tinyint() {
     this.spacer();
-    this.collumn_string += `TINYINT`;
+    this.string += `TINYINT`;
 
-    return this || this.collumn_string;
+    return this || this.string;
   }
 
   static bool() {
     this.spacer();
-    this.collumn_string += `BOOL`;
+    this.string += `BOOL`;
 
-    return this || this.collumn_string;
+    return this || this.string;
   }
 
   static boolean() {
     this.spacer();
-    this.collumn_string += `BOOLEAN`;
+    this.string += `BOOLEAN`;
 
-    return this || this.collumn_string;
+    return this || this.string;
   }
 
   static smallint(size) {
     this.spacer();
-    this.collumn_string += `SMALLINT(${size})`;
+    this.string += `SMALLINT(${size})`;
 
-    return this || this.collumn_string;
+    return this || this.string;
   }
 
   static mediumint(size) {
     this.spacer();
-    this.collumn_string += `MEDIUMINT(${size})`;
+    this.string += `MEDIUMINT(${size})`;
 
-    return this || this.collumn_string;
+    return this || this.string;
   }
 
   static int(size) {
     this.spacer();
-    this.collumn_string += `INT(${size})`;
+    this.string += `INT(${size})`;
 
-    return this || this.collumn_string;
+    return this || this.string;
   }
 
   static integer(size) {
     this.spacer();
-    this.collumn_string += `INTEGER(${size})`;
+    this.string += `INTEGER(${size})`;
 
-    return this || this.collumn_string;
+    return this || this.string;
   }
 
   static bigint(size) {
     this.spacer();
-    this.collumn_string += `BIGINT(${size})`;
+    this.string += `BIGINT(${size})`;
 
-    return this || this.collumn_string;
+    return this || this.string;
   }
 
   static float(size, d) {
     this.spacer();
-    this.collumn_string += `FLOAT(${size}, ${d})`;
+    this.string += `FLOAT(${size}, ${d})`;
 
-    return this || this.collumn_string;
+    return this || this.string;
   }
 
   static double(size, d) {
     this.spacer();
-    this.collumn_string += `DOUBLE(${size}, ${d})`;
+    this.string += `DOUBLE(${size}, ${d})`;
 
-    return this || this.collumn_string;
+    return this || this.string;
   }
 
   static double_precision(size, d) {
     this.spacer();
-    this.collumn_string += `DOUBLE PRECISION(${size}, ${d})`;
+    this.string += `DOUBLE PRECISION(${size}, ${d})`;
 
-    return this || this.collumn_string;
+    return this || this.string;
   }
 
   static decimal(size, d) {
     this.spacer();
-    this.collumn_string += `DECIMAL(${size}, ${d})`;
+    this.string += `DECIMAL(${size}, ${d})`;
 
-    return this || this.collumn_string;
+    return this || this.string;
   }
 
   static text() {
     this.spacer();
-    this.collumn_string += `TEXT`;
+    this.string += `TEXT`;
 
-    return this || this.collumn_string;
+    return this || this.string;
   }
 
   static bit(size) {
     this.spacer();
-    this.collumn_string += `BIT(${size})`;
+    this.string += `BIT(${size})`;
 
-    return this || this.collumn_string;
+    return this || this.string;
   }
 
   static blob(size) {
     this.spacer();
-    this.collumn_string += `BLOB(${size})`;
+    this.string += `BLOB(${size})`;
 
-    return this || this.collumn_string;
+    return this || this.string;
   }
 
   static mediumtext(size) {
     this.spacer();
-    this.collumn_string += `MEDIUMTEXT`;
+    this.string += `MEDIUMTEXT`;
 
-    return this || this.collumn_string;
+    return this || this.string;
   }
 
   static mediumblob(size) {
     this.spacer();
-    this.collumn_string += `MEDIUMBLOB`;
+    this.string += `MEDIUMBLOB`;
 
-    return this || this.collumn_string;
+    return this || this.string;
   }
 
   static longtext() {
     this.spacer();
-    this.collumn_string += `LONGTEXT`;
+    this.string += `LONGTEXT`;
 
-    return this || this.collumn_string;
+    return this || this.string;
   }
 
   static longblob() {
     this.spacer();
-    this.collumn_string += `LONGBLOB`;
+    this.string += `LONGBLOB`;
 
-    return this || this.collumn_string;
+    return this || this.string;
   }
 
   static enum(list) {
     this.spacer();
-    this.collumn_string += `ENUM(${list})`;
+    this.string += `ENUM(${list})`;
 
-    return this || this.collumn_string;
+    return this || this.string;
   }
 
   static set(list) {
     this.spacer();
-    this.collumn_string += `SET(${list})`;
+    this.string += `SET(${list})`;
 
-    return this || this.collumn_string;
+    return this || this.string;
   }
 
   static null(n = true) {
     this.spacer();
-    if (!n) this.collumn_string += 'NOT ';
+    if (!n) this.string += 'NOT ';
 
-    this.collumn_string += `NULL`;
+    this.string += `NULL`;
 
-    return this || this.collumn_string;
+    return this || this.string;
   }
 
   static not_null() {
     this.spacer();
 
-    this.collumn_string += `NOT NULL`;
+    this.string += `NOT NULL`;
 
-    return this || this.collumn_string;
+    return this || this.string;
   }
 
   static primary_key() {
     this.spacer();
-    this.collumn_string += 'PRIMARY KEY';
+    this.string += 'PRIMARY KEY';
 
-    return this || this.collumn_string;
+    return this || this.string;
   }
 
   static date() {
     this.spacer();
-    this.collumn_string += 'DATE';
+    this.string += 'DATE';
 
-    return this || this.collumn_string;
+    return this || this.string;
   }
 
   static datetime(format) {
     this.spacer();
-    this.collumn_string += `DATETIME(${format})`;
+    this.string += `DATETIME(${format})`;
 
-    return this || this.collumn_string;
+    return this || this.string;
   }
 
   static timestamp(format) {
     this.spacer();
-    this.collumn_string += `TIMESTAMP(${format})`;
+    this.string += `TIMESTAMP(${format})`;
 
-    return this || this.collumn_string;
+    return this || this.string;
   }
 
   static time(format) {
     this.spacer();
-    this.collumn_string += `TIME(${format})`;
+    this.string += `TIME(${format})`;
 
-    return this || this.collumn_string;
+    return this || this.string;
   }
 
   static year() {
     this.spacer();
-    this.collumn_string += `YEAR`;
+    this.string += `YEAR`;
 
-    return this || this.collumn_string;
+    return this || this.string;
   }
 
   static auto_increment() {
     this.spacer();
-    this.collumn_string += `AUTO_INCREMENT`;
+    this.string += `AUTO_INCREMENT`;
 
-    return this || this.collumn_string;
+    return this || this.string;
   }
 };
 
