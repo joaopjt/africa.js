4b7dd35f2285a490243ab656ed38d6238c08f347
Joao Julio <joaopjt@gmail.com>
2022-10-01 11:46:58 -0300

    Adding rollup instead of parcel for bundle process

---
+++ rollup.config.js
@@ -0,0 +1,34 @@
+import babel from '@rollup/plugin-babel';
+import minify from 'rollup-plugin-babel-minify';
+import resolve from '@rollup/plugin-node-resolve';
+import json from 'rollup-plugin-json';
+import commonjs from '@rollup/plugin-commonjs';
+import replace from '@rollup/plugin-replace';
+import { uglify } from 'rollup-plugin-uglify';
+
+const babelConfig = { babelrc: true, exclude: 'node_modules/**' };
+const minifyConfig = { comments: false };
+
+export default {
+  input: 'resources/index.js',
+  output: {
+    name: 'index',
+    file: 'dist/index.min.js',
+    format: 'umd'
+  },
+  interop: false,
+  plugins: [
+    replace({
+      preventAssignment: true,
+      "process.env.NODE_ENV": JSON.stringify("development")
+    }),
+    babel(babelConfig),
+    resolve(),
+    commonjs({
+      include: '/node_modules/'
+    }),
+    json(),
+    minify(minifyConfig),
+    uglify()
+  ]
+}
